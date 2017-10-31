df09af8214f922498133fe8f122f8894437fa905
geekybot <uputpal16@gmail.com>
2017-10-31 23:05:34 +0530

    latest

--- LearningSystem.sol
+++ LearningSystem.sol
@@ -29,7 +29,7 @@ contract LearningSystem{
         string[] mentors;
     }
     //Mappings
-
+    
     mapping(address=>Student) student;
     mapping(address=>Evaluator) evaluator;
     Task[] tasks;
@@ -49,33 +49,62 @@ contract LearningSystem{
         return true;
     }
     //function for student submit tasks
-
+    
     function submitTask(address _student_id,string _tasks,bytes32 _technology)payable returns(bool){
-        Task ts;
+        Task memory ts;
         ts.student_id=_student_id;
         ts.task=_tasks;
         ts.technology=_technology;
         tasks.push(ts);
-        Certificate cert;
+        Certificate memory cert;
         cert.student_id=_student_id;
         cert.name=student[_student_id].name;
+        cert.technology=_technology;
+        certificates.push(cert);
+        return true;
     }
     //function for evaluator
     function upvoteTask(address _student_id,uint _task_id) payable returns(bool){
         string name=evaluator[msg.sender].name;
-        tasks[_task_id].upvotes=tasks[_task_id].upvotes+1;
-        tasks[_task_id].mentors[tasks[_task_id].mentors.length++]=name;
-
-
-
-
-        // if(tasks[_task_id].upvotes==5){
-        //     Certificate cert;
-        //     cert.student_id=_student_id;
-        //     cert.name=student[_student_id].name;
-        //     cert.technology=tasks[_task_id].technology;
-
-        // }
+        if(evaluator[msg.sender].miles>=1000){
+            tasks[_task_id].upvotes=tasks[_task_id].upvotes+1;
+            tasks[_task_id].mentors[tasks[_task_id].mentors.length++]=name;
+            certificates[_task_id].mentors[certificates[_task_id].mentors.length++]=name;
+            
+            if(tasks[_task_id].upvotes==5){
+                certificates[_task_id].timestamp=now;
+            }
+            return true;
+        }
+        return false;
     }
-
+    
+    //__________________________________________
+    //getting details
+    //__________________________________________
+    function getStudent(address _student_id) returns (string name){
+        name=student[_student_id].name;
+    }
+    function getEvaluator(address _eval_id) returns (string name, bytes32 technology, uint miles){
+        name=evaluator[_eval_id].name;
+        technology=evaluator[_eval_id].technology;
+        miles=evaluator[_eval_id].miles;
+    }
+    
+    function getTask(uint id) returns (address student_id, string task, bytes32 technology,uint upvotes){
+        Task ts=tasks[id];
+        student_id=ts.student_id;
+        task=ts.task;
+        technology=ts.technology;
+        upvotes=ts.upvotes;
+    }
+    function getlengthMentors(uint id)returns (uint l){
+        l=tasks[id].mentors.length;
+    }
+    function getMentors(uint id, uint index) returns(string name){
+        name=certificates[id].mentors[index];
+    }
+    function getEvaluatorSig(uint id, uint index) returns(string name){
+        name=tasks[id].mentors[index];
+    }
 }
